2b0cb1bb491f453130acef846060a42e946ca0c6
Mauro Zurlo <mauro.e.zurlo@gmail.com>
2025-11-13 00:52:17 -0300

    remove samples

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
 import { BattleModule } from './battle/battle.module';
 
 @Module({
   imports: [CharactersModule, BattleModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
